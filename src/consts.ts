582a16905da98bbbcb5f0e018257c26b6a3a60df
Pacheco <lucaspdroz@gmail.com>
2024-02-05 15:54:18 -0300

    chore: change consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog do Pacheco';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Home - Pacheco\'s Blog';
+export const SITE_DESCRIPTION = 'Welcome to my website! your hub for frontend excellence, UX brilliance, and career wisdom in tech. Delve into UI intricacies, master UX design, and navigate the professional landscape through firsthand accounts. Join us on a concise yet comprehensive journey of development and discovery.';
